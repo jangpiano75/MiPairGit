fffa6b20b4fcf627a148222ab1447a7c3c7945ca
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2022-10-22 12:00:46 +0900

    Update app.R

--- app.R
+++ app.R
@@ -9,6 +9,7 @@ list.of.packages <- c('seqinr', 'shinydashboard', 'dashboardthemes', 'tidyverse'
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
+if(!require('fBasics')) install.packages("fBasics", type = "binary")
 if(!require('BiocParallel')) BiocManager::install("BiocParallel") 
 if(!require('phyloseq')) remotes::install_github('joey711/phyloseq')
 if(!require('biomformat')) remotes::install_github('joey711/biomformat') 
