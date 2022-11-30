e65a7f026b2412345b4cf1733e8aa34923312486
YJ7599 <109124970+YJ7599@users.noreply.github.com>
2022-11-30 23:41:46 +0900

    Update app.R

--- app.R
+++ app.R
@@ -97,7 +97,7 @@ source("Source/MiDataProc.Taxa.Paired.Mult.R")
   HOME_COMMENT3 = ("Comparative analysis between baseline (or reference) and other groups")
   HOME_COMMENT4 = ("Parametric or non-parametric tests for incomplete or complete block designs")
   HOME_COMMENT5 = ("Both ecological (alpha-and beta-diversity) and taxonomic (e.g., phylum, class, order, family, genus, species) analysis")
-  HOME_COMMENT6 = p("Reference: Jang HJ, Koh H, Gu W, Kang B. Interactive web cloud computing and analytics using MiPair for design-based comparative analysis with paired microbiome data (in revision). ", style = "font-size:13pt")
+  HOME_COMMENT6 = p("Reference: Jang H, Koh H, Gu W, Kang B. (2022) Integrative web cloud computing and analytics using MiPair for design-based comparative analysis with paired microbiome data. Scientific Reports 12:20465 https://doi.org/10.1038/s41598-022-25093-6.", style = "font-size:13pt")
   
   INPUT_PHYLOSEQ_COMMENT1 = p("Description:", br(), br(), "This should be an '.Rdata' or '.rds' file, and the data should be in 'phyloseq' format (see ", 
                               a(tags$u("https://bioconductor.org/packages/release/bioc/html/phyloseq.html"), style = "color:red3"),
@@ -299,11 +299,11 @@ source("Source/MiDataProc.Taxa.Paired.Mult.R")
         tabItem(tabName = "home",
                 div(id = "homepage", br(), HOME_COMMENT,
                     tags$ol(
-                      tags$li(HOME_COMMENT1), tags$li(HOME_COMMENT2), tags$li(HOME_COMMENT3), tags$li(HOME_COMMENT4), tags$li(HOME_COMMENT5),
+                      tags$li(HOME_COMMENT1), tags$li(HOME_COMMENT2), tags$li(HOME_COMMENT3), tags$li(HOME_COMMENT4), tags$li(HOME_COMMENT5), tags$li(HOME_COMMENT6),
                       style = "font-size:13pt"), 
                     div(tags$img(src="mipair_workflow.png", height = 900, weight = 500), style =  "text-align: center;"),
                     br(), 
-                    HOME_COMMENT6, 
+                     
                     )),
         
         ##### DATA INPUT ####
